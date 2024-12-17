2748977fa41a3e410591c810c67aa17c1ff87927
davi0063 <clement.david1@etudiant.univ-reims.fr>
2024-12-17 09:41:36 +0100

    JS Recherche

--- assets/medecine/filter.js
+++ assets/medecine/filter.js
@@ -1,1 +1,25 @@
+const inputSearch = document.getElementById('input-search');
+const submitSearch = document.getElementById('submit-search');
+const medicinesContainer = document.getElementById('medicines-container');
 
+if (inputSearch && submitSearch) {
+    inputSearch.addEventListener('keyup', (e) => {
+        e.preventDefault();
+        if (e.key === 'Enter') return searchMedicine(e.target.value);
+    })
+
+    submitSearch.addEventListener('click', (e) => {
+        e.preventDefault();
+        return searchMedicine(inputSearch.value);
+    });
+}
+
+async function searchMedicine(input) {
+    const response = await fetch(`/medicines?search=${input}&containerOnly=1`, {
+        method: 'GET'
+    });
+
+    if(!response.ok) return;
+
+    medicinesContainer.innerHTML = await response.text();
+}
