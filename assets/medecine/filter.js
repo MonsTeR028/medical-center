7352e892edcd304730efbf473b1bf12a00621840
davi0063 <clement.david1@etudiant.univ-reims.fr>
2024-12-17 10:27:12 +0100

    refactorisation JS

--- assets/medecine/filter.js
+++ assets/medecine/filter.js
@@ -5,26 +5,35 @@ const medicinesContainer = document.getElementById('medicines-container');
 if (inputSearch && submitSearch) {
     inputSearch.addEventListener('keyup', (e) => {
         e.preventDefault();
-        if (e.key === 'Enter') return searchMedicine(e.target.value);
+
+        if (e.key === 'Enter') return setQueryString('search', e.target.value);
     })
 
     submitSearch.addEventListener('click', (e) => {
         e.preventDefault();
-        return searchMedicine(inputSearch.value);
+        return setQueryString('search', inputSearch.value);
     });
 }
 
-async function searchMedicine(input) {
-    const response = await fetch(`/medicines?search=${input}&containerOnly=1`, {
+async function setQueryString(key, value) {
+    const queryString = window.location.search;
+    const params = new URLSearchParams(queryString);
+    params.set(key, value);
+
+    if (history.pushState) {
+        const newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${params.toString()}`;
+        window.history.pushState({path: newUrl}, '', newUrl);
+    }
+
+    await filter(params.toString());
+}
+
+async function filter(query) {
+    const response = await fetch(`/medicines?${query}&containerOnly=1`, {
         method: 'GET'
     });
 
     if(!response.ok) return;
 
     medicinesContainer.innerHTML = await response.text();
-
-    if (history.pushState) {
-        const newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?search=${input}`;
-        window.history.pushState({path:newUrl},'',newUrl);
-    }
 }
