e02b3784d25aa24a1024d175a2b570060e60fbd1
peri0060 <romaric.perichard@etudiant.univ-reims.fr>
2024-12-22 14:31:56 +0100

    Complétion de la méthode initialize

--- src/Factory/UserFactory.php
+++ src/Factory/UserFactory.php
@@ -4,7 +4,6 @@ namespace App\Factory;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityRepository;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
 use Zenstruck\Foundry\Persistence\Proxy;
@@ -12,14 +11,32 @@ use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
 
 /**
  * @extends PersistentProxyObjectFactory<User>
+ *
+ * @method        User|Proxy                              create(array|callable $attributes = [])
+ * @method static User|Proxy                              createOne(array $attributes = [])
+ * @method static User|Proxy                              find(object|array|mixed $criteria)
+ * @method static User|Proxy                              findOrCreate(array $attributes)
+ * @method static User|Proxy                              first(string $sortedField = 'id')
+ * @method static User|Proxy                              last(string $sortedField = 'id')
+ * @method static User|Proxy                              random(array $attributes = [])
+ * @method static User|Proxy                              randomOrCreate(array $attributes = [])
+ * @method static UserRepository|ProxyRepositoryDecorator repository()
+ * @method static User[]|Proxy[]                          all()
+ * @method static User[]|Proxy[]                          createMany(int $number, array|callable $attributes = [])
+ * @method static User[]|Proxy[]                          createSequence(iterable|callable $sequence)
+ * @method static User[]|Proxy[]                          findBy(array $attributes)
+ * @method static User[]|Proxy[]                          randomRange(int $min, int $max, array $attributes = [])
+ * @method static User[]|Proxy[]                          randomSet(int $number, array $attributes = [])
  */
-final class UserFactory extends PersistentProxyObjectFactory{
+final class UserFactory extends PersistentProxyObjectFactory
+{
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
      *
      * @todo inject services if required
      */
     private UserPasswordHasherInterface $passwordHasher;
+
     public function __construct(UserPasswordHasherInterface $passwordHasher)
     {
         parent::__construct();
@@ -54,7 +71,9 @@ final class UserFactory extends PersistentProxyObjectFactory{
     protected function initialize(): static
     {
         return $this
-            // ->afterInstantiate(function(User $user): void {})
+            ->afterInstantiate(function (User $user) {
+                $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+            })
         ;
     }
 }
