9217156d20917d190543720ae3f5fd4e4d9cf0f2
peri0060 <romaric.perichard@etudiant.univ-reims.fr>
2024-12-30 17:57:11 +0100

    Configuration de l'affichage d'un médicament : ajout des types d'administration avec un ChoiceLabel et de sa catégorie avec un AssociationField

--- src/Controller/Admin/MedicineCrudController.php
+++ src/Controller/Admin/MedicineCrudController.php
@@ -4,7 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Medicine;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +20,44 @@ class MedicineCrudController extends AbstractCrudController
         return Medicine::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name', 'Nom du médicament'),
+            ChoiceField::new('type', 'Type')->setChoices([
+                'Orale (per os)' => 'oral',
+                'Sublinguale' => 'sublingual',
+                'Rectale' => 'rectal',
+                'Intraveineuse (IV)' => 'intravenous',
+                'Intramusculaire (IM)' => 'intramuscular',
+                'Sous-cutanée (SC)' => 'subcutaneous',
+                'Transdermique' => 'transdermal',
+                'Cutanée' => 'cutaneous',
+                'Oculaire (ophtalmique)' => 'ocular',
+                'Auriculaire (otic)' => 'auricular',
+                'Nasale' => 'nasal',
+                'Vaginale' => 'vaginal',
+                'Pulmonaire (inhalation)' => 'pulmonary',
+                'Intra-artérielle' => 'intra-arterial',
+                'Intra-articulaire' => 'intra-articular',
+                'Intrathécale' => 'intrathecal',
+                'Péridurale' => 'epidural',
+                'Intradermique' => 'intradermal',
+                'Buccale' => 'buccal',
+                'Intranasale' => 'intranasal',
+                'Intraoculaire' => 'intraocular',
+                'Intrapéritonéale' => 'intraperitoneal',
+                'Intraosseuse' => 'intraosseous',
+                ]),
+            TextField::new('dosage', 'Dosage'),
+            MoneyField::new('priceUnit', 'Prix unitaire')
+                ->setCurrency('EUR'),
+            TextareaField::new('description', 'Descriptif')
+                ->hideOnIndex(),
+            AssociationField::new('category', 'Catégories')
+                ->setFormTypeOption('by_reference', false)
+                ->setFormTypeOption('choice_label', 'name'),
         ];
     }
-    */
 }
