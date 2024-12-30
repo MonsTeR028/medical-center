af8f44438a5a1db78d4ecd80d01edf458875eb24
peri0060 <romaric.perichard@etudiant.univ-reims.fr>
2024-12-30 17:21:30 +0100

    Configuration de l'affichage d'un user : ajout du role dans l'affichage

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,13 +17,27 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('Email'),
+            TextField::new('Password')->onlyOnForms()
+                ->setFormType(PasswordType::class)
+                ->setRequired(false)
+                ->setEmptyData('')
+                ->setCustomOption('autocomplete', 'new-password'),
+            TextField::new('FirstName'),
+            TextField::new('LastName'),
+            ArrayField::new('Roles')->formatValue(function ($value) {
+                if (in_array('ROLE_ADMIN', $value)) {
+                    return '<span class="material-symbols-outlined">manage_accounts</span>';
+                } elseif (in_array('ROLE_USER', $value)) {
+                    return '<span class="material-symbols-outlined">person</span>';
+                } else {
+                    return '';
+                }
+            }),
         ];
     }
     */
