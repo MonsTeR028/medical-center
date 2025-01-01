e389fea4e616002c49a634c0780773979560a5ac
peri0060 <romaric.perichard@etudiant.univ-reims.fr>
2025-01-01 17:06:32 +0100

    Transfert du code du controlleur dans le service

--- src/Service/Cart/CartService.php
+++ src/Service/Cart/CartService.php
@@ -6,6 +6,13 @@ class CartService
 {
     public function add(int $id): void
     {
+        $cart = $session->get('cart', []);
+        if (isset($cart[$id])) {
+            ++$cart[$id];
+        } else {
+            $cart[$id] = 1;
+        }
+        $session->set('cart', $cart);
     }
 
     public function remove(int $id): void
