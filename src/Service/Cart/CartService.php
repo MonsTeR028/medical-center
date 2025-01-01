0cdf8113dcf9ef968f800074723b5f2a639c6b6d
peri0060 <romaric.perichard@etudiant.univ-reims.fr>
2025-01-01 17:05:39 +0100

    Déclaration des prototypes des actions

--- src/Service/Cart/CartService.php
+++ src/Service/Cart/CartService.php
@@ -4,4 +4,19 @@ namespace App\Service\Cart;
 
 class CartService
 {
+    public function add(int $id): void
+    {
+    }
+
+    public function remove(int $id): void
+    {
+    }
+
+    public function getCart(): array
+    {
+    }
+
+    public function getTotal(): int
+    {
+    }
 }
