152810e176164ab72f9e9c4022c5b02fd21dd6b2
peri0060 <romaric.perichard@etudiant.univ-reims.fr>
2025-01-01 17:09:00 +0100

    Utilisation de notre attribut dans la méthode add

--- src/Service/Cart/CartService.php
+++ src/Service/Cart/CartService.php
@@ -13,13 +13,13 @@ class CartService
     }
     public function add(int $id): void
     {
-        $cart = $session->get('cart', []);
+        $cart = $this->session->get('cart', []);
         if (isset($cart[$id])) {
             ++$cart[$id];
         } else {
             $cart[$id] = 1;
         }
-        $session->set('cart', $cart);
+        $this->session->set('cart', $cart);
     }
 
     public function remove(int $id): void
