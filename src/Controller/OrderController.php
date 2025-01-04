c96194cae48a2f14df29205cc3d353512aac2e4c
peri0060 <romaric.perichard@etudiant.univ-reims.fr>
2025-01-04 22:19:00 +0100

    Création du formulaire dans l'action index et affichage de celui ci dans son template

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
+use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted("ROLE_USER")]
+#[IsGranted('ROLE_USER')]
 class OrderController extends AbstractController
 {
     #[Route('/order/create', name: 'app_order_index')]
     public function index(): Response
     {
+        $form = $this->createForm(OrderType::class, null, ['user' => $this->getUser()]);
+
         return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+            'form' => $form,
         ]);
     }
 }
