da28eba96e52a876b0b19d8c7892e0c76cda1678
peri0060 <romaric.perichard@etudiant.univ-reims.fr>
2025-01-04 14:21:22 +0100

    Ajout d'une sécurité IsGranted en vérifiant qu'un utilisateur soit connecté pour pouvoir créer une commande

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("ROLE_USER")]
 class OrderController extends AbstractController
 {
     #[Route('/order/create', name: 'app_order_index')]
