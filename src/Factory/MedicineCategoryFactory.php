684efdaccfb98b0b030f9110aac1ff87f45d257b
Karim Rysman <karim.rysman@etudiant.univ-reims.fr>
2025-01-07 00:07:50 +0100

    Factory pour MedicineCategory

---
+++ src/Factory/MedicineCategoryFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\MedicineCategory;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<MedicineCategory>
+ */
+final class MedicineCategoryFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return MedicineCategory::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'name' => self::faker()->text(30),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(MedicineCategory $medicineCategory): void {})
+        ;
+    }
+}
