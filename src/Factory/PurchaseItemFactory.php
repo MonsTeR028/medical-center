6bfd601f764ee846f09f321ba05fc48c275e95ad
Karim Rysman <karim.rysman@etudiant.univ-reims.fr>
2025-01-06 23:09:13 +0100

    Factory pour PurchaseItem

---
+++ src/Factory/PurchaseItemFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\PurchaseItem;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<PurchaseItem>
+ */
+final class PurchaseItemFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return PurchaseItem::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'quantity' => self::faker()->randomNumber(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(PurchaseItem $purchaseItem): void {})
+        ;
+    }
+}
