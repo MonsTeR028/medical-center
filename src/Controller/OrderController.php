e7269203322ddcfb2b0ca8b6a8dbf9993d13795c
peri0060 <romaric.perichard@etudiant.univ-reims.fr>
2025-01-08 21:21:15 +0100

    Ajout de l'action prepare qui s'occupe d'enregistrer la commande validé dans la base de donnée (pas fonctionnelle)

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Form\OrderType;
 use App\Service\Cart\CartService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -13,12 +16,33 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class OrderController extends AbstractController
 {
     #[Route('/order/create', name: 'app_order_index')]
-    public function index(CartService $cartService): Response
+    public function index(LoggerInterface $logger): Response
     {
         $form = $this->createForm(OrderType::class, null, ['user' => $this->getUser()]);
 
         return $this->render('order/index.html.twig', [
             'form' => $form,
         ]);
+    }
+    #[Route('/order/prepare', name: 'app_order_prepare', methods: ['POST'])]
+    public function prepareOrder(Request $request, CartService $cartService, LoggerInterface $logger): Response
+    {
+        $logger->info('tesy');
+        $form = $this->createForm(OrderType::class, null, ['user' => $this->getUser()]);
+        $form->handleRequest($request);
+        dd($form);
+        /*
+        if ($form->isSubmitted() && $form->isValid()) {
+            $dateTime = new \DateTime();
+            $transporter = $form->get('transporters')->getData();
+            $order = new Order();
+            $order->setIdUser($this->getUser()->getId());
+            $order->setOrderDate($dateTime);
+            $order->setStatus('IN PROGRESS');
+            $order->setTotalAmount($cartService->getTotal() + $transporter->getPrice());
+        }
+        */
+        return $this->render('order/prepare.html.twig', []);
+
     }
 }
