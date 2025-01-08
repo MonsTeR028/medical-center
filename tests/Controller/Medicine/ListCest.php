8bbfd70c67814e265d56b8146b9a27a134a3dd18
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-01-09 00:14:49 +0100

    medicines tests

---
+++ tests/Controller/Medicine/ListCest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Controller\Medicine;
+
+use App\Factory\MedicineFactory;
+use App\Tests\Support\ControllerTester;
+
+use function PHPUnit\Framework\assertEquals;
+
+class ListCest
+{
+    public function responseIsOk(ControllerTester $I): void
+    {
+        $I->amOnRoute('app_medicine');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInTitle('Liste des médicaments');
+        $I->seeNumberOfElements('div.medicines div.medicine', 0);
+    }
+
+    public function search(ControllerTester $I): void
+    {
+        $I->amOnRoute('app_medicine');
+        $I->seeResponseCodeIsSuccessful();
+        $I->fillField('search', 'A');
+        $I->click('input#submit-search[type=submit]');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInCurrentUrl('?search=A');
+    }
+}
