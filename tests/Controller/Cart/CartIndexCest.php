b47681dfb4d70aa4722fe1375be66d34cc629b93
davi0063 <clement.david1@etudiant.univ-reims.fr>
2025-01-08 22:34:12 +0100

    début tests panier

---
+++ tests/Controller/Cart/CartIndexCest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Tests\Controller\Cart;
+
+use App\Factory\UserFactory;
+use App\Tests\Support\ControllerTester;
+
+class CartIndexCest
+{
+    public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
+    {
+        $I->amOnRoute('app_cart');
+        $I->seeCurrentRouteIs('app_login');
+    }
+
+    public function responseIsOk(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne();
+        $realUser = $user->_real();
+        $I->amLoggedInAs($realUser);
+
+        $I->amOnRoute('app_cart');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInTitle('Panier');
+        $I->see('Votre Panier', 'h3');
+        $I->see('Votre panier est vide !', 'p');
+    }
+
+
+}
