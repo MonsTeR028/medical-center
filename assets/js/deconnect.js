eadf7f4678c7ea0191b3a6bf3aa6ca6f7a05508f
audi0010 <tristan.audinot@etudiant.univ-reims.fr>
2025-01-09 14:24:57 +0100

    Modification de l'animation du bouton deconnexion

--- assets/js/deconnect.js
+++ assets/js/deconnect.js
@@ -5,22 +5,21 @@ document.addEventListener('DOMContentLoaded', function () {
     if (accountElement && deconnectContainer) {
         accountElement.addEventListener('mouseover', function () {
             deconnectContainer.style.top = '0';
-            deconnectContainer.style.transition = 'ease-in-out 0.3s'
+            deconnectContainer.style.transition = 'ease-in-out 0.5s 1s'
         });
 
         accountElement.addEventListener('mouseleave', function () {
             deconnectContainer.style.top = '-55px';
-            deconnectContainer.style.transition = 'ease-in-out 0.3s'
+            deconnectContainer.style.transition = 'ease-in-out 0.5s'
         });
 
         deconnectContainer.addEventListener('mouseover', function () {
             deconnectContainer.style.top = '0';
-            deconnectContainer.style.transition = 'ease-in-out 0.3s'
         });
 
         deconnectContainer.addEventListener('mouseleave', function () {
             deconnectContainer.style.top = '-55px';
-            deconnectContainer.style.transition = 'ease-in-out 0.3s'
+            deconnectContainer.style.transition = 'ease-in-out 0.5s'
         });
     }
 });
