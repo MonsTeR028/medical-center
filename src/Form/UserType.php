e080d60a72f2025e173a065c6b47060ee3663a1a
Florian Chasseloup <florian.chasseloup@etudiant.univ-reims.fr>
2025-01-11 21:15:08 +0100

    Ajout du champ Catégorie a l'enregistrement d'un utilisateur

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Repository\MedicineCategoryRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -11,6 +12,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
+    private MedicineCategoryRepository $medicineCategoryRepository;
+
+    public function __construct(MedicineCategoryRepository $medicineCategoryRepository)
+    {
+        $this->medicineCategoryRepository = $medicineCategoryRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -19,7 +27,13 @@ class UserType extends AbstractType
             ->add('firstname', \Symfony\Component\Form\Extension\Core\Type\TextType::class, ['empty_data' => ''])
             ->add('lastname', \Symfony\Component\Form\Extension\Core\Type\TextType::class, ['empty_data' => ''])
             ->add('tel', \Symfony\Component\Form\Extension\Core\Type\TextType::class, ['empty_data' => ''])
-            ->add('RRPS', \Symfony\Component\Form\Extension\Core\Type\TextType::class, ['empty_data' => '']);
+            ->add('RRPS', \Symfony\Component\Form\Extension\Core\Type\TextType::class, ['empty_data' => ''])
+            ->add('category', \Symfony\Component\Form\Extension\Core\Type\ChoiceType::class, [
+                'choices' => $this->medicineCategoryRepository->findAll(),
+                'choice_label' => 'name',
+                'choice_value' => 'id',
+                'empty_data' => '',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
