c0433ed67d507d1a0d3f779c9b72a51cab7b51f6
Florian Chasseloup <florian.chasseloup@etudiant.univ-reims.fr>
2025-01-12 19:01:33 +0100

    Fix ListCest.php

--- tests/Controller/Medicine/ListCest.php
+++ tests/Controller/Medicine/ListCest.php
@@ -2,20 +2,43 @@
 
 namespace App\Tests\Controller\Medicine;
 
+use App\Factory\MedicineCategoryFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class ListCest
 {
     public function responseIsOk(ControllerTester $I): void
     {
+        $cat = MedicineCategoryFactory::createOne();
+        $user = UserFactory::createOne([
+            'category' => $cat,
+        ]);
+        $realUser = $user->_real();
+        $I->amLoggedInAs($realUser);
+
         $I->amOnRoute('app_medicine');
         $I->seeResponseCodeIsSuccessful();
         $I->seeInTitle('Liste des médicaments');
         $I->seeNumberOfElements('div.medicines div.medicine', 0);
     }
 
+    public function UserNeedsToBeLogged(ControllerTester $I): void
+    {
+        $I->amOnRoute('app_medicine');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInTitle('Veuillez vous connecter');
+        $I->seeNumberOfElements('div.medicines div.medicine', 0);
+    }
+
     public function search(ControllerTester $I): void
     {
+        $cat = MedicineCategoryFactory::createOne();
+        $user = UserFactory::createOne([
+            'category' => $cat,
+        ]);
+        $realUser = $user->_real();
+        $I->amLoggedInAs($realUser);
         $I->amOnRoute('app_medicine');
         $I->seeResponseCodeIsSuccessful();
         $I->fillField('search', 'A');
